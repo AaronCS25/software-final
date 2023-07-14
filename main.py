08aa8a139af72711b50b8f896b4592154f102d7d
AaronCS25 <aaron.camacho@utec.edu.pe0>
2023-07-13 19:24:02 -0500

    classes Cuenta & Operacion added

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 from fastapi import FastAPI
+from datetime import datetime
+from typing import List
 
 app = FastAPI()
+
+class Cuenta:
+    def __init__(self, numero, saldo, contactos):
+        self.numero = numero
+        self.saldo = saldo
+        self.contactos = contactos
+        self.operaciones = []
+
+class Operacion:
+    def __init__(self, numero_destino, fecha, valor):
+        self.numero_destino = numero_destino
+        self.fecha = fecha
+        self.valor = valor
+

4240c07c5bfcc6042877fbcade0577f0713d398d
AaronCS25 <aaron.camacho@utec.edu.pe0>
2023-07-13 19:28:12 -0500

    bd initialized

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ from typing import List
 
 app = FastAPI()
 
+# Classes
 class Cuenta:
-    def __init__(self, numero, saldo, contactos):
+    def __init__(self, numero, dueno, saldo, contactos):
         self.numero = numero
+        self.dueno = dueno
         self.saldo = saldo
         self.contactos = contactos
         self.operaciones = []
@@ -17,3 +19,8 @@ class Operacion:
         self.fecha = fecha
         self.valor = valor
 
+#List<Cuenta> BD = new List<Cuenta>();
+BD = []
+BD.append(Cuenta("21345", "Arnaldo", 200, ["123", "456"]))
+BD.append(Cuenta("123", "Luisa", 400, ["456"]))
+BD.append(Cuenta("456", "Andrea", 300, ["21345"]))
